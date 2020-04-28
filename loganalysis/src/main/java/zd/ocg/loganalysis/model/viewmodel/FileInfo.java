e26b4c57b7e63901346b95ee052c556eaa1d3711
fancky <709737588@qq.com>
2020-04-28 14:54:43 +0800

    ajax  post get 前后台交互

---
+++ loganalysis/src/main/java/zd/ocg/loganalysis/model/viewmodel/FileInfo.java
@@ -0,0 +1,14 @@
+package zd.ocg.loganalysis.model.viewmodel;
+
+ public class FileInfo
+{
+  private   String filePath;
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
