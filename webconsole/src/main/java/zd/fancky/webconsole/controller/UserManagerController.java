752dc4aadc7c19eec529e60b622de4fa18a661e1
fancky <709737588@qq.com>
2020-08-13 17:01:06 +0800

    WebConsole

---
+++ webconsole/src/main/java/zd/fancky/webconsole/controller/UserManagerController.java
@@ -0,0 +1,14 @@
+package zd.fancky.webconsole.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/UserManager")
+public class UserManagerController {
+    @GetMapping("")
+    public String index() {
+        return "usermanager/index";
+    }
+}
