e1c6ca6e6662930c7af9202cb324c00c9393db77
fancky <709737588@qq.com>
2020-10-07 10:02:18 +0800

    日志整理

---
+++ loganalysis/src/main/java/zd/fancky/loganalysis/controller/HomeController.java
@@ -0,0 +1,27 @@
+package zd.fancky.loganalysis.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther fancky
+ * @Date 2020-10-7 9:17
+ * @Description
+ */
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/logTest")
+    public  void logTest()
+    {
+        //Spring Boot默认的日志级别为INFO
+        //日志级别以及优先级排序: OFF > FATAL > ERROR > WARN > INFO > DEBUG > TRACE > ALL
+        logger.debug("debug");
+        logger.info("info");
+        logger.error("error");
+    }
+}
